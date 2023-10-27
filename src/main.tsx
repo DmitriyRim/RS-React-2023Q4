7b66dcce920995f4787f256401c42fb2d0a5ab0e
Rimfis <dimitri.rim@yandex.ru>
2023-10-27 19:52:53 +0300

    feat: add prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+let a;
+a = 1;
+a = 2;
+console.log(a);
