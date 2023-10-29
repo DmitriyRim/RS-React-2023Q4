e213bf56b0cf356587c5592a36d7d63b87847f87
Rimfis <dimitri.rim@yandex.ru>
2023-10-29 22:07:43 +0300

    feat: add ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,63 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { Component } from 'react';
+import { Search } from './components/Search/Search';
 import './App.css';
+import { Main, MainProps } from './components/Main/Main';
 
-function App() {
-  const [count, setCount] = useState(0);
+interface AppProps {}
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+interface AppState {
+  data: MainProps[];
+  loading: boolean;
+  showError: boolean;
+}
+
+class App extends Component<AppProps, AppState> {
+  state = {
+    data: [],
+    loading: false,
+    showError: false,
+  };
+
+  getContent = (queryString: string = ''): Promise<Response> => {
+    return fetch(`https://swapi.dev/api/people/?search=${queryString}`);
+  };
+
+  buttonHandle = async () => {
+    this.setState({ loading: false });
+    const queryString = localStorage.getItem('search');
+
+    const response = await this.getContent(queryString || '');
+    const data = await response.json();
+
+    this.setState({ data: data.results, loading: true });
+  };
+
+  showError = () => {
+    this.setState({ showError: true });
+  };
+
+  componentDidMount(): void {
+    this.buttonHandle();
+  }
+
+  render() {
+    if (this.state.showError) {
+      throw new Error('Писец');
+    }
+    return (
+      <>
+        <h1>The Star Wars</h1>
+        <button onClick={this.showError}>Error</button>
+        <Search handle={this.buttonHandle} />
+        <hr />
+        {this.state.loading ? (
+          <Main data={this.state.data} />
+        ) : (
+          <div className="loader"></div>
+        )}
+      </>
+    );
+  }
 }
 
 export default App;
