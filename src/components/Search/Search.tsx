20e59bacd9f91611dc06e1f889250215a5b09a2c
Rimfis <dimitri.rim@yandex.ru>
2023-11-07 21:42:09 +0300

    refactor: Search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,39 +1,15 @@
-import { Component } from 'react';
-import './Search.scss';
+import { Form } from 'react-router-dom';
+import { getQueryParameters } from '../../utils/utils';
 
-interface PropsSearch {
-  handle: () => void;
-}
-
-export class Search extends Component<PropsSearch> {
-  public state = {
-    defaultString: '',
-    currentQueryString: '',
-  };
-  public componentDidMount(): void {
-    const string = localStorage.getItem('search');
-
-    if (string) {
-      this.setState({ defaultString: string, currentQueryString: string });
-    }
-  }
-  private searchHandle = (event: React.FormEvent<HTMLInputElement>): void => {
-    let string = event.currentTarget.value;
-
-    string = string.trim();
-    localStorage.setItem('search', string);
-    this.setState({ currentQueryString: string });
-  };
-  render() {
-    return (
-      <div className="search-container">
-        <input
-          type="text"
-          defaultValue={this.state.defaultString}
-          onChange={this.searchHandle}
-        />
-        <button onClick={this.props.handle}>Search</button>
-      </div>
-    );
-  }
-}
+export const Search = () => {
+  return (
+    <Form className="search-container">
+      <input
+        type="text"
+        defaultValue={getQueryParameters('search') || ''}
+        name="search"
+      />
+      <button type="submit">Search</button>
+    </Form>
+  );
+};
