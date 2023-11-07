02112f0ae435b1ebf255a1ac27fc91c2c96673ab
Rimfis <dimitri.rim@yandex.ru>
2023-11-07 21:41:27 +0300

    refactor: Main component

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,38 +1,25 @@
-import { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Users } from '../../types/types';
 
-export interface MainProps {
-  data: {
-    birth_year: string;
-    created: string;
-    edited: string;
-    eye_color: string;
-    films: string[];
-    gender: string;
-    hair_color: string;
-    height: string;
-    homeworld: string;
-    mass: string;
-    name: string;
-    skin_color: string;
-    species: string[];
-    starships: string[];
-    url: string;
-    vehicles: string[];
-  }[];
+export interface Props {
+  data: Users[];
 }
 
-export class Main extends Component<MainProps> {
-  render() {
-    return (
-      <main>
-        <ul>
-          {this.props.data.length
-            ? this.props.data.map((item, index) => (
-                <li key={index}>{item.name}</li>
-              ))
-            : 'not found'}
-        </ul>
-      </main>
-    );
-  }
-}
+export const Main = ({ data }: Props) => {
+  const getLinksUrl = (urlItem: string) => {
+    const url = new URL(urlItem);
+    return url.pathname.slice(11) + location.search;
+  };
+
+  return (
+    <ul>
+      {data.length
+        ? data.map((item, index) => (
+            <NavLink to={getLinksUrl(item.url)} key={index}>
+              {item.name}
+            </NavLink>
+          ))
+        : 'not found'}
+    </ul>
+  );
+};
