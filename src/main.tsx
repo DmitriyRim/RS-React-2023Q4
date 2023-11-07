544c695427166bd1dcb4a99e60031e9342ad86ad
Rimfis <dimitri.rim@yandex.ru>
2023-11-07 21:39:58 +0300

    fix: main.tsx remove app component

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import './index.scss';
 import { ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary.tsx';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes/router.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <App />
+      <RouterProvider router={router} />
     </ErrorBoundary>
   </React.StrictMode>
 );
