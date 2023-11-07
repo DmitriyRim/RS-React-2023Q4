c2fd256999b1872b563e29137fe2d0a2e910609c
Rimfis <dimitri.rim@yandex.ru>
2023-11-07 21:36:46 +0300

    feat: add react router

---
+++ src/routes/router.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from 'react-router-dom';
+import { Root } from '../pages/Root';
+import { Profile } from '../pages/Profile';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        path: '/:userId',
+        element: <Profile />,
+        loader: ({ params }) => {
+          return fetch(`https://swapi.dev/api/people/${params.userId}`);
+        },
+      },
+    ],
+  },
+]);
