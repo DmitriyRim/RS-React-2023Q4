f91f4b325af2a74587cad2c2a74f4c57b96bad44
Rimfis <dimitri.rim@yandex.ru>
2023-11-12 22:17:18 +0300

    refactor: typing for useLoaderData

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -1,4 +1,4 @@
-export interface Users {
+export interface People {
   birth_year: string;
   created: string;
   edited: string;
@@ -15,4 +15,102 @@ export interface Users {
   starships: string[];
   url: string;
   vehicles: string[];
+  title?: string;
+}
+
+export interface Planets {
+  climate: string;
+  created: string;
+  diameter: string;
+  edited: string;
+  films: string[];
+  gravity: string;
+  name: string;
+  orbital_period: string;
+  population: string;
+  residents: string[];
+  rotation_period: string;
+  surface_water: string;
+  terrain: string;
+  url: string;
+  title?: string;
+}
+
+export interface Films {
+  characters: string[];
+  created: string;
+  director: string;
+  edited: string;
+  episode_id: string;
+  opening_crawl: string;
+  planets: string[];
+  producer: string;
+  release_date: string;
+  species: string[];
+  starships: string[];
+  title: string;
+  url: string;
+  vehicles: string[];
+  name?: string;
+}
+
+export interface Starships {
+  MGLT: string;
+  cargo_capacity: string;
+  consumables: string;
+  cost_in_credits: string;
+  created: string;
+  crew: string;
+  edited: string;
+  hyperdrive_rating: string;
+  length: string;
+  manufacturer: string;
+  max_atmosphering_speed: string;
+  model: string;
+  name: string;
+  passengers: string;
+  films: string[];
+  pilots: string[];
+  starship_class: string;
+  url: string;
+  title?: string;
+}
+
+export interface Vehicles {
+  cargo_capacity: string;
+  consumables: string;
+  cost_in_credits: string;
+  created: string;
+  crew: string;
+  edited: string;
+  length: string;
+  manufacturer: string;
+  max_atmosphering_speed: string;
+  model: string;
+  name: string;
+  passengers: string;
+  pilots: string[];
+  films: string[];
+  url: string;
+  vehicle_class: string;
+  title?: string;
+}
+
+export interface Species {
+  average_height: string;
+  average_lifespan: string;
+  classification: string;
+  created: string;
+  designation: string;
+  edited: string;
+  eye_colors: string;
+  hair_colors: string;
+  homeworld: string;
+  language: string;
+  name: string;
+  people: string[];
+  films: string[];
+  skin_colors: string;
+  url: string;
+  title?: string;
 }
