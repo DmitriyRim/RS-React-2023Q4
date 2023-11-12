e12eb4942a99b26fe719a7aa2e7e3d6637ae06ef
Rimfis <dimitri.rim@yandex.ru>
2023-11-12 22:19:51 +0300

    refactor: search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,13 +1,29 @@
 import { Form } from 'react-router-dom';
 import { getQueryParameters } from '../../utils/utils';
+import './Search.scss';
+import { useEffect, useState } from 'react';
 
 export const Search = () => {
+  const [defaultValue, setDefaultValue] = useState('');
+
+  useEffect(() => {
+    const query = getQueryParameters('search');
+
+    if (query) {
+      localStorage.setItem('search', query);
+      setDefaultValue(query);
+    } else {
+      setDefaultValue(localStorage.getItem('search') || '');
+    }
+  }, []);
+
   return (
     <Form className="search-container">
       <input
         type="text"
-        defaultValue={getQueryParameters('search') || ''}
+        defaultValue={defaultValue || ''}
         name="search"
+        onChange={(e) => localStorage.setItem('search', e.currentTarget.value)}
       />
       <button type="submit">Search</button>
     </Form>
